6e45d3dd4acac23479d1c2cd0ce2ef5579e3f92c
DINFM1 Oumar Sabaly <oumar.sabaly@localhost.localdomain>
2025-01-16 09:07:01 +0100

    mon troixieme commit

--- myJavaScript.js
+++ myJavaScript.js
@@ -1,3 +1,4 @@
+/*
 console.log('Hello, world!');
 
 const a = 12;
@@ -6,7 +7,7 @@ let b = 45;
 b += a;
 
 console.log("resultat de a + b = " + b);
-
+*/
 let aPerson = { 
 	name: 'Jean',
 	age: 35,
@@ -33,3 +34,28 @@ const operand2 = 45;
 let result = operand + operand2;
 
 console.log("resultat de" + operand + "+" + operand2 + "=" + result);
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Dimanche";
+    break;
+  case 1:
+    day = "Lundi";
+    break;
+  case 2:
+     day = "Mardi";
+    break;
+  case 3:
+    day = "Mercredi";
+    break;
+  case 4:
+    day = "Jeudi";
+    break;
+  case 5:
+    day = "Vendredi";
+    break;
+  case 6:
+    day = "Samedi";
+}
+console.log("Aujourd'hui nous sommes " + day);
